9b2e2a81f3a65374b0f07049c0575bb4c708e5ee
Yiding He <yidinghe2012@gmail.com>
2022-07-12 15:30:34 -0400

    day 06, rating view on draw.

--- app/src/main/java/com/candibell/androidcustomview/day06/Main06Activity.kt
+++ app/src/main/java/com/candibell/androidcustomview/day06/Main06Activity.kt
@@ -2,11 +2,12 @@ package com.candibell.androidcustomview.day06
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.candibell.androidcustomview.R
 
-class Main06Activity: AppCompatActivity() {
+class Main06Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_main_06)
     }
 }
--- app/src/main/java/com/candibell/androidcustomview/day06/RatingBar.kt
+++ app/src/main/java/com/candibell/androidcustomview/day06/RatingBar.kt
@@ -1,26 +1,75 @@
-package com.candibell.androidcustomview.day06;
+package com.candibell.androidcustomview.day06
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.util.AttributeSet;
-import android.view.View;
+import android.content.Context
+import kotlin.jvm.JvmOverloads
+import android.graphics.Bitmap
+import android.view.MotionEvent
+import android.content.res.TypedArray
+import com.candibell.androidcustomview.R
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
 
-import androidx.annotation.Nullable;
-
-public class RatingBar extends View {
-
-    private Bitmap mStarNormalBitmap, mStarFocusBitmap;
-    private int mGradeNumber = 5;
-
-    public RatingBar(Context context) {
-        this(context, null);
+class RatingBar @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+    private val mStarNormalBitmap: Bitmap
+    private val mStarFocusBitmap: Bitmap
+    private var mGradeNumber = 5
+    private var mCurrentGrade = 0
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        // 高度, 一张图片的高度
+        val height = mStarNormalBitmap.height
+        val width = mStarNormalBitmap.width * mGradeNumber
+        setMeasuredDimension(width, height)
     }
 
-    public RatingBar(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs, 0);
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        for (i in 0 until mGradeNumber) {
+            // i * 星星的宽度
+            val x = i * mStarFocusBitmap.width
+            val top = paddingTop
+            if (mCurrentGrade > i) {
+                canvas.drawBitmap(mStarFocusBitmap, x.toFloat(), top.toFloat(), null)
+            } else {
+                canvas.drawBitmap(mStarNormalBitmap, x.toFloat(), top.toFloat(), null)
+            }
+        }
     }
 
-    public RatingBar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        // event.getX() 相对于当前控件的位置. event.getRawX() 获取屏幕的x位置
+        when (event.action) {
+            MotionEvent.ACTION_MOVE -> {
+                //            case MotionEvent.ACTION_UP: // 抬起
+                val moveX = event.x
+                var currentGrade = (moveX / mStarNormalBitmap.width + 1).toInt()
+                if (currentGrade < 0) {
+                    currentGrade = 0
+                }
+                if (currentGrade > mGradeNumber) {
+                    currentGrade = mGradeNumber
+                }
+                mCurrentGrade = currentGrade
+                invalidate() //尽量减少onDraw的调用.
+            }
+        }
+        // false不消费, 第一次down false, down以后的事件进不来.
+        return true
+    }
+
+    init {
+        val array = context.obtainStyledAttributes(attrs, R.styleable.RatingBar)
+        val startNormalId = array.getResourceId(R.styleable.RatingBar_starNormal, 0)
+        val startFocusId = array.getResourceId(R.styleable.RatingBar_starFocus, 0)
+        mGradeNumber = array.getInt(R.styleable.RatingBar_gradeNumber, mGradeNumber)
+        mStarNormalBitmap = BitmapFactory.decodeResource(resources, startNormalId)
+        mStarFocusBitmap = BitmapFactory.decodeResource(resources, startFocusId)
+        array.recycle()
     }
 }
